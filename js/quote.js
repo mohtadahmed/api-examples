c2e621d8440e80de6a908350890c2177f83852bb
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-12 00:30:13 +0600

    practicing kanye api

---
+++ js/quote.js
@@ -0,0 +1,11 @@
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuotes(data))
+};
+
+
+const displayQuotes = (quote) => {
+    const blockQuote = document.getElementById('quote');
+    blockQuote.innerText = quote.quote;
+};
