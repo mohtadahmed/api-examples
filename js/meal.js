484081b2accd018b9b212d3223996ca9a939a79e
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-12 15:45:31 +0600

    searching meal option with search field

---
+++ js/meal.js
@@ -0,0 +1,36 @@
+const loadMeals = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = ``;
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `
+        mealContainer.appendChild(mealDiv);
+    });
+}
+
+
+const searchFood = () =>{
+    // const searchButton = document.getElementById('btn-search');
+    const searchField = document.getElementById('search-input-field');
+    const searchText = searchField.value;
+    loadMeals(searchText);
+    searchText.value = '';
+}
+
+// loadMeals();
