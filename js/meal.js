0ceab8887332ee60dfa257ac8de9b6a8dbb9501d
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-13 01:37:44 +0600

    food details showing part added

--- js/meal.js
+++ js/meal.js
@@ -8,11 +8,12 @@ const loadMeals = (search) => {
 const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container');
     mealContainer.innerHTML = ``;
+    // console.log(meals);
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadMealDetails(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
@@ -26,11 +27,38 @@ const displayMeals = meals => {
 
 
 const searchFood = () =>{
-    // const searchButton = document.getElementById('btn-search');
     const searchField = document.getElementById('search-input-field');
     const searchText = searchField.value;
     loadMeals(searchText);
     searchField.value = '';
 }
 
-// loadMeals();
+const loadMealDetails = mealId => {
+    const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+
+const displayMealDetails = meal =>{
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = ``;
+    const mealDiv = document.createElement('div');
+    mealDiv.innerHTML = `
+    <div class="card">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <hr>
+            <p class="card-text">Origin: ${meal.strArea}</p>
+            <p class="card-text">Category: ${meal.strCategory}</p>
+            <p class="card-text">Type of Food: ${meal.strTags}</p>
+            <a href="#" class="btn btn-primary">Select Food</a>
+        </div>
+    </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
+
+loadMeals('');
